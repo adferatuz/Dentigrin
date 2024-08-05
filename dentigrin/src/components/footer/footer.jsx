7d528bb70cd62797d22d4fb55a085730be789913
Adrian Fernandez <viadfeur@gmail.com>
2024-08-04 21:55:19 -0500

    Archivo footer.jsx modificado y actualizado junto con su archivo style.css

--- dentigrin/src/components/footer/footer.jsx
+++ dentigrin/src/components/footer/footer.jsx
@@ -1,7 +1,24 @@
 import './styles.css'
+import iconInstagram from '../../assets/icons/logotipo-de-instagram.png'
+import iconfb from '../../assets/icons/fbIcon.png'
+import iconYT from '../../assets/icons/youtube.png'
 
 export default function Footer (){
     return(
-            <div className="footer"></div>         
+            <div className="footer">
+                <div className='footer-info'>
+                    <p>©2024 DENTIGRIN</p>
+                    <p>Todos los derechos reservados</p>
+                </div>
+                <div className="footer-info">
+                    <p>Terminos y condiciones</p>
+                    <p>Politica de privacidad</p>
+                </div>
+                <div className="footer-icons">
+                    <div className="icon"><img src={iconInstagram} alt="icono de instagram" /></div>
+                    <div className="icon"><img src={iconfb} alt="" /></div>
+                    <div className="icon"><img src={iconYT} alt="" /></div>
+                </div>
+            </div>         
     )
 }
