889374edfe77a94b5e9c42728d4a6b61977515e7
TatianaAstaiza <Tatica310@hotmail.com>
2024-08-09 20:51:44 -0500

    creando la primer pull request

--- dentigrin/src/components/handleErrors/manejador404.jsx
+++ dentigrin/src/components/handleErrors/manejador404.jsx
@@ -5,6 +5,7 @@ export default function Manejador404 (){
     return(
         <div>
             <h1>Pagina No Encontrada</h1>
+            <h1>haciendo el primer pull request</h1>
         </div>
     )
 }
