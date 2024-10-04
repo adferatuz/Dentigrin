2417ae85f49805b3bbd400c1c18afc3d50d7f6c5
Adrian Fernandez <viadfeur@gmail.com>
2024-10-04 18:29:44 -0500

    Modificacion de archivos pacienteController.js, pacienteService.js, pacienteValidation.js, Serapacion de responsabilidades segun principios SOLID

--- dentigrin_backend/src/controllers/pacienteController.js
+++ dentigrin_backend/src/controllers/pacienteController.js
@@ -1,12 +1,20 @@
 const Paciente = require('@models/paciente/paciente');
 const pacienteService = require('@services/pacienteService');
-const { validatePaciente } = require('../domain/validations/pacienteValidation');
+const { validatePaciente, validatePacienteId } = require('../domain/validations/pacienteValidation');
 
 //Obtener todos los pacientes
 exports.getPacientes = async (req, res) => {
     try {
-        const pacientes = await Paciente.findAll();
+
+      //Llamada al servicio para crear un nuevo paciente
+      const pacientes = await pacienteService.getPacientes();
+
+      if(pacientes){
         res.json(pacientes);
+      } else{
+        // Lanza una respuesta http 204 que significa no-content
+        res.status(204).json({ message: 'Aun no ahi pacientes' });
+      }
 
     } catch (error){
         res.status(500).json({ message: error.message});
@@ -16,12 +24,21 @@ exports.getPacientes = async (req, res) => {
 // Obtener un paciente por ID
 exports.getPacienteById = async (req, res) => {
     try {
-      const paciente = await Paciente.findByPk(req.params.id);
+
+      //Validacion de datos que llegan desde el lado del cliente.
+      const {error} = validatePacienteId( req.params.id );
+      if (error) {
+        return res.status(404).json({message: error.details[0].message});
+      }
+
+      //Llamada al servicio para buscar un paciente por id.
+      const paciente = await pacienteService.getPacienteById(req.params.id);
       if (paciente) {
         res.json(paciente);
       } else {
         res.status(404).json({ message: 'Paciente no encontrado' });
       }
+      
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -53,15 +70,22 @@ exports.getPacienteById = async (req, res) => {
   // Actualizar un paciente
   exports.updatePaciente = async (req, res) => {
     try {
-      const [updated] = await Paciente.update(req.body, {
-        where: { id_paciente: req.params.id }
-      });
-      if (updated) {
-        const updatedPaciente = await Paciente.findByPk(req.params.id);
-        res.status(200).json(updatedPaciente);
+
+      //Validacion de datos que llegan desde el lado del cliente.
+      const { error } = validatePaciente(req.body);
+      if (error) {
+        return res.status(400).json({message: error.details[0].message});
+      }
+
+      //Llamada al servicio para actualizar un paciente
+      const actualizado = await pacienteService.updatePaciente(req.body, req.params.id);
+      if (actualizado) {
+        const pacienteActualizado = await pacienteService.getPacienteById(req.params.id);
+        res.status(200).json(pacienteActualizado);
       } else {
         res.status(404).json({ message: 'Paciente no encontrado' });
       }
+
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
@@ -70,9 +94,15 @@ exports.getPacienteById = async (req, res) => {
   // Eliminar un paciente
   exports.deletePaciente = async (req, res) => {
     try {
-      const deleted = await Paciente.destroy({
-        where: { id_paciente: req.params.id }
-      });
+
+      //Validacion de datos que llegan desde el lado del cliente.
+      const {error} = validatePacienteId( req.params.id );
+      if (error) {
+        return res.status(404).json({message: error.details[0].message});
+      }
+      
+      //Llamada al servicio para eliminar un paciente
+      const deleted = await pacienteService.deletePaciente(req.params.id);
       if (deleted) {
         res.status(204).json({ message: 'Paciente eliminado' });
       } else {
--- dentigrin_backend/src/domain/services/pacienteService.js
+++ dentigrin_backend/src/domain/services/pacienteService.js
@@ -1,5 +1,6 @@
 const Paciente = require('@models/paciente/paciente');
 
+//Servicio para crear una persistencia de datos del ORM Paciente.
 exports.createPaciente = async (data) => {
     try {
         const nuevoPaciente = await Paciente.create(data);
@@ -8,3 +9,49 @@ exports.createPaciente = async (data) => {
         throw new Error(`Error al crear el paciente: ${error.message}`);       
     }
 };
+
+//Servicio para listar todos los pacientes de la base de datos.
+exports.getPacientes = async () => {
+    try {
+        const pacientes = await Paciente.findAll();
+        return pacientes;
+    } catch (error) {
+        throw new Error(`Error listar pacientes: ${error.message}`); 
+    }
+}
+
+//Servicio para buscar un paciente por id.
+exports.getPacienteById = async (id) => {
+    try {
+        const paciente = await Paciente.findByPk(id);
+        return paciente;
+    } catch (error) {
+        throw new Error(`Error al encontrar el paciente: ${error.message}`);
+    }
+}
+
+//Servicio para actualizar un paciente
+exports.updatePaciente = async (data, id) => {
+    try {
+        const [updated] = await Paciente.update(data, {
+            where: { id: id }
+          });
+        return updated;
+    } catch (error) {
+        throw new Error(`Error al encontrar el paciente: ${error.message}`);
+    }
+}
+
+//Servicio para  eliminar un paciente
+exports.deletePaciente = async (id) => {
+    try {
+        const pacienteEliminado = await Paciente.destroy({
+            where: { id: id }
+          });
+        return  pacienteEliminado;
+    } catch (error) {
+        throw new Error(`Error al eliminar el paciente: ${error.message}`);
+        
+    }
+}
+
